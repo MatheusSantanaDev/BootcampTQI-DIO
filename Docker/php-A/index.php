37dfc8b4bfbcb567a65392e2c3b8ecbfcffc7a76
MatheusSantanaDev <matheus.santanadearaujo@outlook.com>
2023-05-15 22:55:54 -0300

    Armazenamento de dados entre docker e Git

---
+++ Docker/php-A/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Exemplo Apache</title>
+</head>
+<body>
+<h1> OK !! Apache funcionando!!!!! </h1>
+</body>
+</html>
+
+
+
+
+<?php
+phpinfo();
+?>
